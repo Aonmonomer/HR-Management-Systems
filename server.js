954b765ff351d1b9b99b2b7ced6b24cae39ceadc
Seng Kit Mun <cellkit11@gmail.com>
2022-07-28 23:00:45 -0400

    setting up deployment

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname}/client/build/index.html`)
-})
+// app.get('/*', (req, res) => {
+//   res.sendFile(`${__dirname}/client/build/index.html`)
+// })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
